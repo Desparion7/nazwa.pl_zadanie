6374e3f2a78a070bb9cb9683c37d7cc3332e0545
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2024-05-22 15:23:14 +0200

    remove empty spaces

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-	// Menu-mobile
+	// Mobile Menu
 	const burgerIcon = document.querySelector('.nav__burger');
 	const popupMenu = document.getElementById('popupMenu');
 	const closePopup = document.getElementById('closePopup');
 
-	burgerIcon.addEventListener('click', function () {
-		popupMenu.style.display = 'flex';
+	const togglePopupMenu = (show) => {
+		popupMenu.style.display = show ? 'flex' : 'none';
+	};
+
+	burgerIcon.addEventListener('click', () => togglePopupMenu(true));
+	closePopup.addEventListener('click', (event) => {
+		event.preventDefault();
+		togglePopupMenu(false);
 	});
 
-	closePopup.addEventListener('click', function (event) {
-		event.preventDefault();
-		popupMenu.style.display = 'none';
-	});
-	// Question popup
+	// Question Popup
 	const questionButton = document.getElementById('questionButton');
 	const questionPopup = document.getElementById('questionPopup');
 	const closeQuestion = document.getElementById('closeQuestion');
@@ -21,37 +23,36 @@ document.addEventListener('DOMContentLoaded', function () {
 	const confirmationMessage = document.getElementById('confirmationMessage');
 	const questionPopupOverlay = document.getElementById('questionPopupOverlay');
 
-	questionButton.addEventListener('click', function () {
-		questionPopup.style.display = 'flex';
-	});
+	const toggleQuestionPopup = (show) => {
+		questionPopup.style.display = show ? 'flex' : 'none';
+	};
 
-	closeQuestion.addEventListener('click', function (event) {
-		event.preventDefault();
-		questionPopup.style.display = 'none';
-	});
-	questionPopupOverlay.addEventListener('click', function (event) {
-		event.preventDefault();
-		questionPopup.style.display = 'none';
-	});
-
-	BtnAnswerCompany.addEventListener('click', function (event) {
-		event.preventDefault();
-		questionPopup.style.display = 'none';
+	const showConfirmationMessage = () => {
 		confirmationMessage.style.display = 'block';
 		setTimeout(() => {
 			confirmationMessage.style.display = 'none';
 		}, 3500);
-	});
-	BtnAnswerPrivate.addEventListener('click', function (event) {
+	};
+
+	questionButton.addEventListener('click', () => toggleQuestionPopup(true));
+	closeQuestion.addEventListener('click', (event) => {
 		event.preventDefault();
-		questionPopup.style.display = 'none';
-		confirmationMessage.style.display = 'block';
-		setTimeout(() => {
-			confirmationMessage.style.display = 'none';
-		}, 3500);
+		toggleQuestionPopup(false);
+	});
+	questionPopupOverlay.addEventListener('click', (event) => {
+		event.preventDefault();
+		toggleQuestionPopup(false);
 	});
 
-	// brutto-netto prices
+	[BtnAnswerCompany, BtnAnswerPrivate].forEach((btn) => {
+		btn.addEventListener('click', (event) => {
+			event.preventDefault();
+			toggleQuestionPopup(false);
+			showConfirmationMessage();
+		});
+	});
+
+	// Brutto-Netto Prices
 	const toggleSwitch = document.getElementById('toggleSwitch');
 	const priceValue = document.getElementById('priceValue');
 	const pricePl = document.getElementById('pricePl');
@@ -73,95 +74,61 @@ document.addEventListener('DOMContentLoaded', function () {
 		},
 	};
 
-	toggleSwitch.addEventListener('change', function () {
+	toggleSwitch.addEventListener('change', () => {
 		const priceType = toggleSwitch.checked ? 'brutto' : 'netto';
 		priceValue.textContent = prices[priceType].value;
 		pricePl.textContent = prices[priceType].pl;
 		priceEu.textContent = prices[priceType].eu;
 		priceCom.textContent = prices[priceType].com;
 	});
+
+	// Footer Year
+	document.getElementById('currentYear').textContent = new Date().getFullYear();
 });
+
 // Section 3 Slider
-
-var testim = document.getElementById('testim'),
-	testimDots = Array.prototype.slice.call(
-		document.getElementById('testim-dots').children
-	),
-	testimContent = Array.prototype.slice.call(
-		document.getElementById('testim-content').children
-	),
-	testimleftArrow = document.getElementById('left-arrow'),
-	testimRightArrow = document.getElementById('right-arrow'),
-	testimSpeed = 4500,
-	currentSlide = 0,
-	currentActive = 0,
-	testimTimer,
-	touchStartX = 0,
-	touchEndX = 0;
-
 window.onload = function () {
-	// Testim Script
-	function playSlide(slide) {
-		for (var k = 0; k < testimDots.length; k++) {
-			testimContent[k].classList.remove('active');
-			testimContent[k].classList.remove('inactive');
-			testimDots[k].classList.remove('active');
-		}
-		if (slide < 0) {
-			slide = currentSlide = testimContent.length - 1;
-		}
-		if (slide > testimContent.length - 1) {
-			slide = currentSlide = 0;
-		}
-		if (currentActive != currentSlide) {
-			testimContent[currentActive].classList.add('inactive');
-		}
+	const testim = document.getElementById('testim');
+	const testimDots = Array.from(document.getElementById('testim-dots').children);
+	const testimContent = Array.from(document.getElementById('testim-content').children);
+	const testimLeftArrow = document.getElementById('left-arrow');
+	const testimRightArrow = document.getElementById('right-arrow');
+	const testimSpeed = 4500;
+	let currentSlide = 0;
+	let currentActive = 0;
+	let testimTimer;
+	let touchStartX = 0;
+	let touchEndX = 0;
+
+	const playSlide = (slide) => {
+		testimContent.forEach((content, index) => {
+			content.classList.remove('active', 'inactive');
+			testimDots[index].classList.remove('active');
+		});
+
+		if (slide < 0) slide = currentSlide = testimContent.length - 1;
+		if (slide >= testimContent.length) slide = currentSlide = 0;
+
+		if (currentActive !== currentSlide) testimContent[currentActive].classList.add('inactive');
 		testimContent[slide].classList.add('active');
 		testimDots[slide].classList.add('active');
 
 		currentActive = currentSlide;
 
 		clearTimeout(testimTimer);
-		testimTimer = setTimeout(function () {
-			playSlide((currentSlide += 1));
-		}, testimSpeed);
-	}
-	// arrow active change slide
-	testimleftArrow.addEventListener('click', function () {
-		playSlide((currentSlide -= 1));
-	});
-	testimRightArrow.addEventListener('click', function () {
-		playSlide((currentSlide += 1));
-	});
+		testimTimer = setTimeout(() => playSlide(++currentSlide), testimSpeed);
+	};
 
-	for (var l = 0; l < testimDots.length; l++) {
-		testimDots[l].addEventListener('click', function () {
-			playSlide((currentSlide = testimDots.indexOf(this)));
-		});
-	}
-	// Touch events to change slide on swipe
-	testim.addEventListener('touchstart', function (event) {
-		touchStartX = event.changedTouches[0].screenX;
-	});
+	testimLeftArrow.addEventListener('click', () => playSlide(--currentSlide));
+	testimRightArrow.addEventListener('click', () => playSlide(++currentSlide));
+	testimDots.forEach((dot, index) => dot.addEventListener('click', () => playSlide(currentSlide = index)));
 
-	testim.addEventListener('touchmove', function (event) {
-		touchEndX = event.changedTouches[0].screenX;
-	});
-
-	testim.addEventListener('touchend', function () {
-		if (touchStartX - touchEndX > 50) {
-			playSlide((currentSlide += 1)); // Swipe left
-		} else if (touchEndX - touchStartX > 50) {
-			playSlide((currentSlide -= 1)); // Swipe right
-		}
+	testim.addEventListener('touchstart', (event) => touchStartX = event.changedTouches[0].screenX);
+	testim.addEventListener('touchmove', (event) => touchEndX = event.changedTouches[0].screenX);
+	testim.addEventListener('touchend', () => {
+		if (touchStartX - touchEndX > 50) playSlide(++currentSlide); // Swipe left
+		if (touchEndX - touchStartX > 50) playSlide(--currentSlide); // Swipe right
 	});
 
 	playSlide(currentSlide);
 };
-
-// Footer Year
-function displayCurrentYear() {
-	var currentYear = new Date().getFullYear();
-	document.getElementById('currentYear').textContent = currentYear;
-}
-document.addEventListener('DOMContentLoaded', displayCurrentYear);
